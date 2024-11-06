6d9ff526ac95e63afaf42ed8978257c44088602e
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-11-05 17:07:21 -0800

    Add files via upload

---
+++ nestedForLoop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	for(int week=1; week<=3; week++){
+		for(int days=1; days<=7; days++)
+		{
+			cout<<"Week-"<<week<<" Day: "<<days<<endl;
+		}
+		cout<<endl;
+	}
+}
